7bd3ad5497fde12fa6a5d9b2f0ce008e9668a28b
wonsakku <kws1044@gmail.com>
2023-11-04 17:12:00 +0900

    project init

---
+++ src/test/java/sample/demo/CafekioskApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafekioskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
